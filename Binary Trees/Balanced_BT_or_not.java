d7ffd6c0f1330ed4c8887ab942b0044e8754bfb1
Kalapi Patel <103921193+Kalapipatel@users.noreply.github.com>
2025-09-03 11:25:03 +0530

    Create Balanced_BT_or_not.java

---
+++ Binary Trees/Balanced_BT_or_not.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int fun(TreeNode root){
+        if(root == null) return 0;
+
+        int left = fun(root.left);
+        if(left == -1) return -1;
+        int right = fun(root.right);
+        if(right == -1) return -1;
+
+        if(Math.abs(left - right) > 1) return -1;
+        return 1 + Math.max(left, right);
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        if(fun(root) == -1) return false;
+        else return true;
+    }
+}
