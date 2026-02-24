555b825cb6e41ee968b1e43e7401fd8652940fb2
Kalapi Patel <103921193+Kalapipatel@users.noreply.github.com>
2026-02-24 09:56:13 +0530

    Update 08_Distinct Subsequences.java

--- DP/DP on Strings/08_Distinct Subsequences.java
+++ DP/DP on Strings/08_Distinct Subsequences.java
@@ -114,3 +114,26 @@ class Solution {
         return prev[m];  
     }
 }
+
+
+// By using only 1 array
+class Solution {
+    public int numDistinct(String s, String t) {
+        int n = s.length();    
+        int m = t.length();  
+        int prev[] = new int[m+1];
+
+        prev[0] = 1;
+
+        for(int i=1; i<=n; i++){
+            for(int j=m; j>=1; j--){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    prev[j] = prev[j-1] + prev[j];
+                }
+            }
+
+        }
+
+        return prev[m];  
+    }
+}
