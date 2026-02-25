d6bb34b5ab960309de2aca0d055839ee9fa6997f
Kalapi Patel <103921193+Kalapipatel@users.noreply.github.com>
2026-02-25 11:37:58 +0530

    Create 02_Best Time to Buy and Sell Stock 2.java

---
+++ DP/DP on Stocks/02_Best Time to Buy and Sell Stock 2.java
@@ -0,0 +1,64 @@
+/*
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can sell and buy the stock multiple times on the same day, ensuring you never hold more than one share of the stock.
+
+Find and return the maximum profit you can achieve.
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Total profit is 4 + 3 = 7.
+Example 2:
+
+Input: prices = [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+Total profit is 4.
+Example 3:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.
+ 
+
+Constraints:
+
+1 <= prices.length <= 3 * 104
+0 <= prices[i] <= 104
+*/
+
+
+// Memoization
+class Solution {
+    public int fun(int idx, int buy, int prices[], int dp[][]){
+        if(idx == prices.length) return 0;
+
+        int profit = 0;
+        if(dp[idx][buy] != -1) return dp[idx][buy];
+        if(buy == 1){
+            profit = Math.max(-prices[idx] + fun(idx+1, 0, prices, dp),
+                                0 + fun(idx+1, 1, prices, dp));
+        }
+        else{
+            profit = Math.max(prices[idx] + fun(idx+1, 1, prices, dp),
+                                0 + fun(idx+1, 0, prices, dp));
+        }
+
+        return dp[idx][buy] = profit;
+    }
+
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int dp[][] = new int[n][2];
+
+        for(int x[] : dp){
+            Arrays.fill(x, -1);
+        }
+
+        return fun(0, 1, prices, dp);
+    }
+}
