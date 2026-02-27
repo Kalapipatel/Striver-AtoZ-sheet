9cb855752caf5f43979900228b0ca522cc841ffa
Kalapi Patel <103921193+Kalapipatel@users.noreply.github.com>
2026-02-27 10:23:15 +0530

    Update 02_Best Time to Buy and Sell Stock 2.java

--- DP/DP on Stocks/02_Best Time to Buy and Sell Stock 2.java
+++ DP/DP on Stocks/02_Best Time to Buy and Sell Stock 2.java
@@ -62,3 +62,66 @@ class Solution {
         return fun(0, 1, prices, dp);
     }
 }
+
+
+// Tbaulation
+class Solution {
+
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int dp[][] = new int[n+1][2];
+        dp[n][0] = dp[n][1] = 0;
+
+        for(int i=n-1; i>=0; i--){
+            for(int j=0; j<=1; j++){
+                int profit = 0;
+
+                if(j == 1){
+                    profit = Math.max(-prices[i] + dp[i+1][0],
+                                0 + dp[i+1][1]);
+                }
+                else{
+                    profit = Math.max(prices[i] + dp[i+1][1],
+                                        0 + dp[i+1][0]);
+                }
+
+                dp[i][j] = profit;
+            }
+        }
+
+        return dp[0][1];
+    }
+}
+
+
+// space optimization
+class Solution {
+
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int ahed[] = new int[2];
+        ahed[0] = ahed[1] = 0;
+
+        for(int i=n-1; i>=0; i--){
+            int curr[] = new int[2];
+            for(int j=0; j<=1; j++){
+                int profit = 0;
+
+                if(j == 1){
+                    profit = Math.max(-prices[i] + ahed[0],
+                                0 + ahed[1]);
+                }
+                else{
+                    profit = Math.max(prices[i] + ahed[1],
+                                        0 + ahed[0]);
+                }
+
+                curr[j] = profit;
+            }
+
+            ahed = curr;
+        }
+
+        return ahed[1];
+    }
+}
